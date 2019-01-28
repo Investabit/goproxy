8f6f4b7a0910667820beb77f9920a4ee47fc4514
dataBaseError <heron.joseph@gmail.com>
2019-01-28 14:42:38 -0500

    Rever hanlding completely

--- https.go
+++ https.go
@@ -115,16 +115,16 @@ func (proxy *ProxyHttpServer) handleHttps(w http.ResponseWriter, r *http.Request
 		targetTCP, targetOK := targetSiteCon.(*net.TCPConn)
 		proxyClientTCP, clientOK := proxyClient.(*net.TCPConn)
 		if targetOK && clientOK {
-			go func() {
-				var wg sync.WaitGroup
-				wg.Add(2)
-				go copyAndClose(ctx, targetTCP, proxyClientTCP, &wg)
-				go copyAndClose(ctx, proxyClientTCP, targetTCP, &wg)
-				wg.Wait()
-				proxyClientTCP.Close()
-				targetTCP.Close()
+			// go func() {
+			// 	var wg sync.WaitGroup
+			// 	wg.Add(2)
+			go copyAndClose(ctx, targetTCP, proxyClientTCP) //, &wg)
+			go copyAndClose(ctx, proxyClientTCP, targetTCP) //, &wg)
+			// 	wg.Wait()
+			// 	proxyClientTCP.Close()
+			// 	targetTCP.Close()
 
-			}()
+			// }()
 		} else {
 			go func() {
 				var wg sync.WaitGroup
@@ -361,14 +361,14 @@ func copyOrWarn(ctx *ProxyCtx, dst io.Writer, src io.Reader, wg *sync.WaitGroup)
 	wg.Done()
 }
 
-func copyAndClose(ctx *ProxyCtx, dst, src *net.TCPConn, wg *sync.WaitGroup) {
+func copyAndClose(ctx *ProxyCtx, dst, src *net.TCPConn) { //, wg *sync.WaitGroup) {
 	if _, err := io.Copy(dst, src); err != nil {
 		ctx.Warnf("Error copying to client: %s", err)
 	}
 
 	dst.CloseWrite()
 	src.CloseRead()
-	wg.Done()
+	// wg.Done()
 }
 
 func dialerFromEnv(proxy *ProxyHttpServer) func(network, addr string, r *http.Request) (net.Conn, error) {
